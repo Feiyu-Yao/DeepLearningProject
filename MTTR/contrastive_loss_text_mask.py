82388b1e0f82ab8caba47649efa3949309fdb795
Yanru Chen <yanruc@bu.edu>
2022-04-19 23:47:15 -0400

    add loss file

---
+++ MTTR/contrastive_loss_text_mask.py
@@ -0,0 +1,66 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+import numpy as np
+
+# every operation should be pytorch
+'''
+Group text and mask as a pair insteads of concatnating them.
+Text need to be upsampled to the same length as mask.
+if want to implement cross-video comparison, need to use NTXentLoss:
+    https://github.com/KevinMusgrave/pytorch-metric-learning/issues/179
+'''
+
+class TMContrastiveLoss(nn.Module):
+    def __init__(self, temperature=0.5):
+        super(TMContrastiveLoss, self).__init__()
+        self.total_loss = 0
+        self.temperature = temperature
+        
+    def forward(self, prediction_mask, positive_encoded_txt, negative_encoded_txt, device):
+        '''
+        "pred_masks": Tensor of dim [time, batch_size, num_queries, H, W] with the predicted masks logits
+        make pairs
+        for mask in outputs:
+            for word in original:
+                make positive pairs
+            for word in inverse:
+                make negative pairs
+        for each mask in outputs:
+            compute contrastive loss
+        '''
+        positive, negative = [], []
+        for i in range(len(prediction_mask)):
+            for j in range(len(positive_encoded_txt)):
+                if (i == j):
+                    mask, ptext, ntext = prediction_mask[i], positive_encoded_txt[j], negative_encoded_txt[j]
+                    mask_text = torch.cat((mask, ptext), dim=0)
+                    positive.append(mask_text)
+                    
+                    mask_text = torch.cat((mask, ntext), dim=0)
+                    negative.append(mask_text)
+                    
+        positive_pairs, negative_pairs = [], []
+        cos = nn.CosineSimilarity(dim=0, eps=1e-6)
+        # group and duplicate all positive pairs
+        for i in range(len(positive)):
+            p, n = torch.flatten(positive[i]), torch.flatten(negative[i])
+            
+            # pad p or n in case they are not the same length
+            mask = torch.zeros(abs(p.shape[0] - n.shape[0])).to(device)
+            if (p.shape[0] > n.shape[0]):
+                n = torch.cat((n, mask), dim=0)
+            else:
+                p = torch.cat((p, mask), dim=0)
+            
+            # import pdb; pdb.set_trace()     
+            positive_pairs.append(torch.exp(cos(p, p)/self.temperature))
+            negative_pairs.append(torch.exp(cos(p, n)/self.temperature))
+            
+        total = torch.stack([torch.stack(negative_pairs).sum(dim=0), torch.stack(positive_pairs).sum(dim=0)]).sum(dim=0)
+            
+        # compute loss
+        for p in positive_pairs:
+            self.total_loss += - torch.log(torch.divide(p, total))
+        
+        return self.total_loss
--- MTTR/trainer.py
+++ MTTR/trainer.py
@@ -26,6 +26,7 @@ from torch.optim.lr_scheduler import MultiStepLR
 import misc as utils
 from models import build_model
 from models.swin_transformer import compute_mask
+from contrastive_loss_text_mask import TMContrastiveLoss
 
 
 class Trainer:
@@ -127,10 +128,12 @@ class Trainer:
                 self.sampler_train.set_epoch(self.epoch)
             total_epoch_loss = 0
             loss_sums_dict = {k: 0 for k in self.criterion.weight_dict.keys()}
+
             for batch_dict in tqdm(self.data_loader_train, disable=not utils.is_main_process()):
                 samples = batch_dict['samples'].to(self.device)
                 targets = to_device(batch_dict['targets'], self.device)
-                text_queries = batch_dict['text_queries']
+                text_queries_original = batch_dict['text_queries_original']
+                text_queries_inverse = batch_dict['text_queries_inverse']
 
                 # keep only the valid targets (targets of frames which are annotated). for example, in a2d-sentences
                 # only the center frame in each window is annotated.
@@ -138,9 +141,21 @@ class Trainer:
                 targets = [targets[i] for i in valid_indices.tolist()]
 
                 with amp.autocast(enabled=self.config.enable_amp):
-                    outputs = self.model(samples, valid_indices, text_queries)
+                    outputs, prediction_mask, (positive_encoded_txt, negative_encoded_txt) \
+                        = self.model(samples, valid_indices, (text_queries_original, text_queries_inverse))
+                    
+                    # add contrastive loss here
+                    contrastive_loss = ContrastiveLoss()
+                    ct_loss = contrastive_loss(prediction_mask, positive_encoded_txt, negative_encoded_txt, self.device)
+                    
+                    # add contrastive loss to the dictionary
                     loss_dict = self.criterion(outputs, targets)
+                    loss_dict['contrastive_learn'] = ct_loss
+                    
+                    # add contrastive loss's weight to the dictionary
                     weight_dict = self.criterion.weight_dict
+                    weight_dict['contrastive_learn'] = 3
+                    
                     losses = sum(loss_dict[k] * weight_dict[k] for k in loss_dict.keys() if k in weight_dict)
 
                 # reduce losses over all GPUs for logging purposes
@@ -182,7 +197,8 @@ class Trainer:
             if self.is_main_process:
                 if self.dataset_name == 'a2d_sentences':
                     mAP_score = eval_metrics.get('mAP 0.5:0.95')
-                    self.save_checkpoint(mAP_score)
+                    # self.save_checkpoint(mAP_score)
+                    self.save_checkpoint(total_epoch_loss)
                 else:  # refer-youtube-vos:
                     self.save_checkpoint(total_epoch_loss)
                 eval_metrics.update({'epoch': self.epoch, 'epoch_loss': total_epoch_loss})
@@ -201,13 +217,13 @@ class Trainer:
         for batch_dict in tqdm(self.data_loader_val, disable=not self.is_main_process):
             samples = batch_dict['samples'].to(self.device)
             targets = to_device(batch_dict['targets'], self.device)
-            text_queries = batch_dict['text_queries']
+            text_queries_original = batch_dict['text_queries_original']
+            text_queries_inverse = batch_dict['text_queries_inverse']
 
             # keep only the valid targets (targets of frames which are annotated):
             valid_indices = torch.tensor([i for i, t in enumerate(targets) if None not in t]).to(self.device)
             targets = [targets[i] for i in valid_indices.tolist()]
-
-            outputs = self.model(samples, valid_indices, text_queries)
+            outputs, _, _ = self.model(samples, valid_indices, text_queries_original)
             outputs.pop('aux_outputs', None)
 
             outputs, targets = flatten_temporal_batch_dims(outputs, targets)
@@ -227,24 +243,26 @@ class Trainer:
             gathered_pred_lists = utils.all_gather(predictions)
             predictions = [p for p_list in gathered_pred_lists for p in p_list]
         eval_metrics = {}
-        if self.is_main_process:
-            coco_gt = COCO(self.config.dataset_coco_gt_format_path)
-            coco_pred = coco_gt.loadRes(predictions)
-            coco_eval = COCOeval(coco_gt, coco_pred, iouType='segm')
-            coco_eval.params.useCats = 0  # ignore categories as they are not predicted in ref-vos task
-            coco_eval.evaluate()
-            coco_eval.accumulate()
-            coco_eval.summarize()
-            ap_labels = ['mAP 0.5:0.95', 'AP 0.5', 'AP 0.75', 'AP 0.5:0.95 S', 'AP 0.5:0.95 M', 'AP 0.5:0.95 L']
-            ap_metrics = coco_eval.stats[:6]
-            eval_metrics = {l: m for l, m in zip(ap_labels, ap_metrics)}
-            if self.config.calculate_precision_and_iou_metrics:
-                precision_at_k, overall_iou, mean_iou = calculate_precision_at_k_and_iou_metrics(coco_gt, coco_pred)
-                eval_metrics.update({f'P@{k}': m for k, m in zip([0.5, 0.6, 0.7, 0.8, 0.9], precision_at_k)})
-                eval_metrics.update({'overall_iou': overall_iou, 'mean_iou': mean_iou})
-            print(eval_metrics)
-        if self.distributed:
-            dist.barrier()  # sync all processes before starting a new epoch or exiting
+        # if self.is_main_process:
+        #     coco_gt = COCO(self.config.dataset_coco_gt_format_path)
+        #     coco_pred = coco_gt.loadRes(predictions)
+        #     # import pdb; pdb.set_trace()
+        #     coco_eval = COCOeval(coco_gt, coco_pred, iouType='segm')
+        #     coco_eval.params.useCats = 0  # ignore categories as they are not predicted in ref-vos task
+        #     coco_eval.evaluate()
+        #     coco_eval.accumulate()
+        #     coco_eval.summarize()
+        #     ap_labels = ['mAP 0.5:0.95', 'AP 0.5', 'AP 0.75', 'AP 0.5:0.95 S', 'AP 0.5:0.95 M', 'AP 0.5:0.95 L']
+        #     ap_metrics = coco_eval.stats[:6]
+        #     eval_metrics = {l: m for l, m in zip(ap_labels, ap_metrics)}
+        #     if self.config.calculate_precision_and_iou_metrics:
+        #         precision_at_k, overall_iou, mean_iou = calculate_precision_at_k_and_iou_metrics(coco_gt, coco_pred)
+        #         eval_metrics.update({f'P@{k}': m for k, m in zip([0.5, 0.6, 0.7, 0.8, 0.9], precision_at_k)})
+        #         eval_metrics.update({'overall_iou': overall_iou, 'mean_iou': mean_iou})
+        #     print(eval_metrics)
+        # if self.distributed:
+        #     dist.barrier()  # sync all processes before starting a new epoch or exiting
+        print()
         return eval_metrics
 
     @torch.no_grad()
@@ -255,7 +273,7 @@ class Trainer:
             samples = batch_dict['samples'].to(self.device)
             valid_indices = torch.arange(len(samples.tensors)).to(self.device)
             text_queries = batch_dict['text_queries']
-            outputs = self.model(samples, valid_indices, text_queries)
+            outputs, _, _ = self.model(samples, valid_indices, text_queries)
             videos_metadata = batch_dict['videos_metadata']
             sample_shape_with_padding = samples.tensors.shape[-2:]
             preds_by_video = self.postprocessor(outputs, videos_metadata, sample_shape_with_padding)
@@ -297,7 +315,8 @@ class Trainer:
         self.epoch = checkpoint['epoch'] + 1  # the epoch after the one saved is about to begin
         self.total_epochs = checkpoint['total_epochs']
         if self.dataset_name == 'a2d_sentences':
-            self.best_mAP = checkpoint['best_mAP']
+            self.best_loss = checkpoint['best_loss']
+            # self.best_mAP = checkpoint['best_mAP']
         else:  # refer-youtube-vos
             self.best_loss = checkpoint['best_loss']
         model_without_ddp = self.model.module if isinstance(self.model, DDP) else self.model
@@ -320,11 +339,19 @@ class Trainer:
             'grad_scaler_state_dict': self.grad_scaler.state_dict()
         }
         if self.dataset_name == 'a2d_sentences':
-            is_best_mAP = epoch_score > self.best_mAP
-            if is_best_mAP:
-                self.best_mAP = epoch_score
+            # import pdb; pdb.set_trace()
+            # is_best_mAP = epoch_score > self.best_mAP
+            # if is_best_mAP:
+            #     self.best_mAP = epoch_score
+            #     is_best = True
+            # checkpoint_dict['best_mAP'] = self.best_mAP
+            print("epoch_score: ", epoch_score, "best_loss: ", self.best_loss)
+            is_best_loss = epoch_score < self.best_loss
+            if is_best_loss:
+                self.best_loss = epoch_score
                 is_best = True
-            checkpoint_dict['best_mAP'] = self.best_mAP
+            checkpoint_dict['best_loss'] = self.best_loss
+            
         else:  # refer-youtube-vos
             is_best_loss = epoch_score < self.best_loss
             if is_best_loss:
